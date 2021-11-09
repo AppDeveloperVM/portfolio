bc06e7bd225a5b956c38d590bc6f477c88274fc9
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-09 12:26:30 +0100

    changed theme options

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
+import "@fontsource/exo-2/300.css"
+
 import { ChakraProvider } from "@chakra-ui/provider";
+//import theme from "@chakra-ui/theme";
+import { extendTheme } from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+    //think about dark / light mode
+    fonts: {
+        heading: "Exo 2",
+    }
+})
+
 
 import  Layout  from "../components/layouts/main";
 
 const Website = ({Component,pageProps,router}) => {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Layout router={router}>
                 <Component {...pageProps} key={router.route}/>
             </Layout>
