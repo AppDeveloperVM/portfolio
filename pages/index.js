784dd2041d72fc65b544e58390df762ab2176571
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-09 14:10:16 +0100

    updated index js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,51 @@
-import { Container,Box,Heading } from "@chakra-ui/react"
+import { Container,Box,Heading, SimpleGrid, HStack, Icon,Button,IconButton,Link,  Avatar } 
+from "@chakra-ui/react"
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
 const Page = () => {
 
 
     return (
     <Container>
-        <Box borderRadius="lg" bg="grey" p={3} align="center">
+        <Box borderRadius="lg" bg="grey" p={3} mb={6} align="center">
         <div>Hello,Im a web developer</div>
         </Box>
         
         <Box display={{ md: 'flex'}}>
-            <Box flexGrow={1}>
-                <Heading as="h2" variant="page-title">
-                    Víc Maree R
-                </Heading>
-            </Box>
+            <SimpleGrid columns={2} spacing={10}>
+                <Box flexGrow={1}>
+                
+                    <Heading as="h2" variant="page-title">
+                        Víc Maree R
+                    </Heading>
+   
+                   
+                    <p>
+                        Web Developer 
+                    </p>
+                    
+                </Box>
+                <Box flexGrow={1}>
+                    <Avatar direction="right" mr={10} size="xl" name="Víc M" src="https://bit.ly/dan-abramov" />
+                </Box>
+            </SimpleGrid>
         </Box>
+        <HStack>
+            <Box
+            as="button">
+                <Link href="https://github.com/AppDeveloperVM" isExternal>
+                    <IconButton aria-label="GitHub" icon={<BsGithub />} /> 
+                </Link>
+            </Box>
+
+            <Box
+            as="button">
+                <Link href="https://www.linkedin.com/in/víctor-maree-redondo-476a91136/" isExternal>
+                    <IconButton aria-label="GitHub" icon={<BsLinkedin />} /> 
+                </Link>
+            </Box>
+        </HStack>
+
     </Container> 
     )
     
