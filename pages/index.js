b0c78109083da94d0d27218e1efdd7530a2af4eb
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-09 12:26:52 +0100

    added some base content with styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { Container,Box,Heading } from "@chakra-ui/react"
 const Page = () => {
+
+
     return (
-        <div>Hello</div>
+    <Container>
+        <Box borderRadius="lg" bg="grey" p={3} align="center">
+        <div>Hello,Im a web developer</div>
+        </Box>
+        
+        <Box display={{ md: 'flex'}}>
+            <Box flexGrow={1}>
+                <Heading as="h2" variant="page-title">
+                    Víc Maree R
+                </Heading>
+            </Box>
+        </Box>
+    </Container> 
     )
+    
 }
 
 export default Page
