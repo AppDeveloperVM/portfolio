0bd281f4900ea89920af92c1251f24362395cfb1
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-14 17:13:41 +0100

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Navbar from '../layouts/navbar.js'
+import Navbar from '../navbar.js'
 import { Box, Container} from '@chakra-ui/react'
 
-const Main = ({ children, router}) => {
+const Main = ({ children, router }) => {
     return (
         <Box as="main" pb={8}>
             <Head>
