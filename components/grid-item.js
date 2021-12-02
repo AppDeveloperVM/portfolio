26fe16bfa136d8118003ef8305d768e99614f646
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-02 13:25:52 +0100

    module grid-item for work

---
+++ components/grid-item.js
@@ -0,0 +1,63 @@
+import { Global } from "@emotion/react"
+import { Box, Text, LinkBox, LinkOverlay,Badge } from "@chakra-ui/react"
+import NextLink from 'next/link'
+import Image from 'next/image'
+
+
+var badges_list = {
+    list: function(badges) {
+        ["white", "white", "primary.700", "primary.700"].forEach(element => {
+        <Badge variant='outline' colorScheme='green' fontSize='0.7em' fontWeight='normal'>{element}</Badge> 
+       });
+    }
+ };   
+
+
+export const GridItem = ({ children, href, title, thumbnail }) => (
+    <Box w="100%" align="center">
+        <LinkBox cursor="pointer">
+            <Image 
+            src={thumbnail}
+            
+            alt={title}
+            className="grid-item-thumbnail"
+            placeholder="blur"
+            loading="lazy" 
+            blurDataURL="/500x400.png/09f/fff"
+            width="600px" height="400px"
+            />
+            <LinkOverlay href={href} target="_blank">
+                <Text mt={2}>{title}</Text>
+            </LinkOverlay>
+            <Text fontSize={14}>{children}</Text>
+        </LinkBox>
+    </Box>
+)
+
+export const WorkGridItem = ({ children, id, title, thumbnail, badges  }) => (
+    <Box w="100%" align="center">
+        <NextLink href={`/works/${id}`}>
+            <LinkBox cursor="pointer">
+                <Image src={thumbnail}  alt={title} className="grid-item-thumbnail" 
+                placeholder="blur" blurDataURL="/500x400.png/09f/fff" width="600px" height="400px" objectFit='cover'/>
+                <LinkOverlay href={`/works`}>
+                    <Text mt={2} fontSize={20} >
+                        {title}
+                    </Text>
+                    { badges_list.list(badges) }
+                </LinkOverlay>
+                
+                <Text fontSize={14} >{children}</Text>
+            </LinkBox>
+        </NextLink>
+    </Box>
+)
+
+export const GridItemStyle = () => (
+    <Global styles={`
+    .grid-item-thumbnail {
+        border-radius: 12px;
+    }
+    `}
+    />
+)
