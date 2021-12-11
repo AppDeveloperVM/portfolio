e4c6bae743bf0e60c722b66b2a00abe71563a5a1
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-11 14:21:11 +0100

    Update develop.js

--- pages/develop.js
+++ pages/develop.js
@@ -1,6 +1,7 @@
 import { Container,Box,Heading, Text,ListItem ,UnorderedList, SimpleGrid,Center, Stack, HStack, Icon,Button,IconButton,Link,  Avatar } 
 from "@chakra-ui/react"
 import Section from '../components/section';
+import { WorkGridItem, GridItem } from "../components/grid-item";
 
 const Develop = () => {
 
@@ -8,18 +9,15 @@ const Develop = () => {
     <Container maxW='container.xl'>
         <Section delay={0.1}>
         
-        <SimpleGrid mt="10" spacing={5}>
         
         <Heading as="h3" variant="section-title" size="md" textDecoration="underline">
             Mi Desarrollo
         </Heading>
-
-        <Stack direction={["column", "row"]} spacing="24px" mt="10">
-            <Box w="250px" h="400px"  bg="gray.500" flexBasis={{ base: "50%", xs: "100%" }}>
-            </Box>
-            <Box w="250px" h="400px"  bg="gray.500" flexBasis={{ base: "50%", xs: "100%" }}>
-            </Box>
-        </Stack>
+        <SimpleGrid mt="10" spacing={5} columns={[1]}>
+        
+            <WorkGridItem  id="peritoline" title="Cosplay Planning App" url="https://github.com/AppDeveloperVM/cosplay-planning-app"  badges={["white", "white", "primary.700", "primary.700"]} >
+                App para la planificación de eventos enfocados al cosplay
+            </WorkGridItem>
 
         </SimpleGrid>
         </Section>
