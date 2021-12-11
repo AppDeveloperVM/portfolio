f0b310178ff325bd66daa1cda9c3d9710a0f1ff3
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-11 14:21:07 +0100

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -28,7 +28,6 @@ const Works = () => {
 
             <SimpleGrid  columns={[1]}  gap={6} spacing={10} mt="10">
     
-                
                 <Box>
                     <Heading as="h4" size="md" align="center" padding={3}>
                         <Link isExternal href="https://www.softline.es/web/">SoftLine Informática</Link>
@@ -40,21 +39,19 @@ const Works = () => {
 
                 </Box>
             </SimpleGrid>
-            <SimpleGrid  columns={[1,1,2]}  gap={6} spacing={10} mt="10"  >
-                <Box>
-                    <Heading as="h4" size="md" align="center" padding={3}>
+
+            <Heading as="h4" size="md" align="center" padding={3} mt="10">
                     <Link href="http://www.horanova.es/" isExternal>Hora Nova ( <Link isExternal href="https://www.gruposerra.com/">Grupo Serra</Link> )</Link> - <Link href="https://www.ultimahora.es/" is External>Última Hora</Link>
                     </Heading>
+            <SimpleGrid  columns={[1,1,2]}  gap={6} spacing={10}   >
+             
+                <WorkGridItem  id="clubdelsuscriptor" title="Club del suscriptor" thumbnail="/images/Clubdelsuscriptor.png" url="https://www.softline.es/web/portfolio/view/2/peritoline-aplicacion-de-gestion-integral-de-despachos-periciales"  badges={["white", "white", "primary.700", "primary.700"]} >
+                    Web dedicada a los eventos de un grupo selecto
+                </WorkGridItem>
 
-                    <WorkGridItem  id="clubdelsuscriptor" title="Club del suscriptor" thumbnail="/images/Clubdelsuscriptor.png" url="https://www.softline.es/web/portfolio/view/2/peritoline-aplicacion-de-gestion-integral-de-despachos-periciales"  badges={["white", "white", "primary.700", "primary.700"]} >
-                        Web dedicada a los eventos de un grupo selecto
-                    </WorkGridItem>
-
-                    <WorkGridItem  id="novaproducciones" title="Nova Producciones" thumbnail="/images/novaProducciones.png" url="https://www.softline.es/web/portfolio/view/2/peritoline-aplicacion-de-gestion-integral-de-despachos-periciales"  badges={["white", "white", "primary.700", "primary.700"]} >
-                        Web dedicada a los eventos de un grupo selecto
-                    </WorkGridItem>
-
-                </Box>
+                <WorkGridItem  id="novaproducciones" title="Nova Producciones" thumbnail="/images/novaProducciones.png" url="https://www.softline.es/web/portfolio/view/2/peritoline-aplicacion-de-gestion-integral-de-despachos-periciales"  badges={["white", "white", "primary.700", "primary.700"]} >
+                    Web dedicada a los eventos de un grupo selecto
+                </WorkGridItem>
 
             </SimpleGrid>
 
