22feacbcac8d95ab25d83349808968dcb76d9681
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2021-12-14 21:10:26 +0100

    added animation to layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import "@fontsource/signika-negative/300.css"
 import { ChakraProvider } from "@chakra-ui/provider";
+import Fonts from '../components/Fonts'
 import theme from '../lib/theme'
-
-
+import { AnimatePresence } from "framer-motion";
 import  Layout  from "../components/layouts/main";
 
 const Website = ({Component,pageProps,router}) => {
     return (
         <ChakraProvider theme={theme}>
+            <Fonts />
             <Layout router={router}>
+                <AnimatePresence exitBeforeEnter initial={true}>
                 <Component {...pageProps} key={router.route}/>
+                </AnimatePresence>
             </Layout>
         </ChakraProvider>
     )
