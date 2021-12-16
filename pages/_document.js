3f27b4c587ffa707b850da50410b954a965aacb2
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2021-12-16 20:03:06 +0100

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class Document extends NextDocument {
         return (
             <Html lang="en">
                 <Head />
+                <link rel="icon" href="/images/favicon_classic.png"></link>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
                     <Main />
