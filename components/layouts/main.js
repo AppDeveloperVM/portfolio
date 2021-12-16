ac7739615768522ce1403fc2549e49e1693c9305
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2021-12-16 19:48:36 +0100

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Portfolio</title>
+                <title>Vic Maree R -  Homepage</title>
             </Head>
 
             <Navbar path={router.asPath} />
