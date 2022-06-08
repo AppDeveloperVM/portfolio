e9b5efe9e224d0f2b961e45343395ca6087a3117
VMTechy <vicmr1994@gmail.com>
2022-06-08 20:51:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ const Page = () => {
             Yo puedo ayudarte!
         </Heading>
 
-        <NextLink href='mailto:vicmr1994@gmail.com?subject=Propuesta%20de%20desarrollo%20web&body=Estoy%20interesado%20en%20pedir%20presupuesto%20para%20un%20proyecto%20web.'>
+        <NextLink href="mailto:vicmr1994@gmail.com?subject=Propuesta%20de%20desarrollo%20web&body=Estoy%20interesado%20en%20pedir%20presupuesto%20para%20un%20proyecto%20web">
             <Button colorScheme='teal' size='sm' mt="5" rightIcon={<EmailIcon/>}>
             Pongámonos en Contacto 
             </Button>
