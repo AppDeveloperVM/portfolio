04d4745460377243b19923134e58286c769ba4bb
VMTechy <vicmr1994@gmail.com>
2022-06-08 21:08:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -116,13 +116,10 @@ const Page = () => {
             Yo puedo ayudarte!
         </Heading>
 
-       
-
-        <Box>
-            <Link href="mailto:vicmr1994@gmail.com?subject=Propuesta%20de%20desarrollo%20web&body=Estoy%20interesado%20en%20pedir%20presupuesto%20para%20un%20proyecto%20web" variant="concealed" >
-            Pongámonos en Contacto <IconButton aria-label="GitHub" icon={<EmailIcon/>} bg={useColorModeValue('gray.300','#313134')} /> 
-            </Link>
-        </Box>
+            <Button href="mailto:vicmr1994@gmail.com?subject=Propuesta%20de%20desarrollo%20web&body=Estoy%20interesado%20en%20pedir%20presupuesto%20para%20un%20proyecto%20web" colorScheme='teal' size='sm' mt="5" rightIcon={<EmailIcon/>}>
+            Pongámonos en Contacto <IconButton aria-label="email" icon={<EmailIcon/>} bg={useColorModeValue('gray.300','#313134')} /> 
+            </Button>
+      
     </Section>
     </Container> 
     </Layout>
